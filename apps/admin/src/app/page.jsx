ad8e5bd087d9abb0b4e41951f226bf4ed6bb2ae8
jasminbhesaniyajb <jasminbhesaniyajb@gmail.com>
2025-04-29 17:53:22 +0530

    inital commit

---
+++ apps/admin/src/app/page.jsx
@@ -0,0 +1,13 @@
+"use client";
+import { Button } from "ui-components/ui/button";
+import { NoDataFound } from "ui-components/ui/no-data-found";
+
+export default function Home() {
+  return (
+    <div className="p-4">
+      <h1 className="text-blue-500 font-bold">Admin panel</h1>
+      <Button>Button</Button>
+      {/* <NoDataFound /> */}
+    </div>
+  );
+}
---
+++ packages/ui-components/ui/no-data-found.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export function NoDataFound() {
+  return (
+    <div className='text-red-500'>No Data Found!</div>
+  )
+}
